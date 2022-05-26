ef3e8886c5d12bd1fc082987415a661e60c3ca0b
ganeshAir <91529291+ganeshAir@users.noreply.github.com>
2022-05-26 23:07:03 +0530

    Added support for dynamic strings

--- Telereso/Classes/Extensions/StringExtension.swift
+++ Telereso/Classes/Extensions/StringExtension.swift
@@ -9,6 +9,14 @@ extension String{
    public func remoteLocale() -> String {
         Telereso.getRemoteString(self)
     }
+    
+    public func remoteLocaleWith(args: [CVarArg]) -> String {
+        Telereso.getRemoteString(self, args: args)
+    }
+    
+    func stringWithParams(_ args: [CVarArg]) -> String {
+        return String(format: self, arguments: args)
+    }
 }
 
 
